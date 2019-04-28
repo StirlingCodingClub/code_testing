ff7404e0aa4d9d496c109adc3724e11299801ee4
Brad Duthie <brad.duthie@gmail.com>
2019-04-28 22:02:10 +0100

    Add simple temperature conversion file and a test file for it

---
+++ test_temp_conversion.R
@@ -0,0 +1,29 @@
+library(testthat);
+source("temp_conversion.R");
+
+test_that("Fahrenheit to Celsius", {
+  
+  temp_C <- F_to_C(50);
+  
+  # Test that the result is numeric
+  expect_that( is.numeric(temp_C), equals(TRUE) );
+  
+  # Test that the result is the correct value
+  expect_that( temp_C, equals(10) );
+})
+
+
+test_that("Celsius to Fahrenheit", {
+  
+  temp_F <- C_to_F(10);
+  
+  # Test that the result is numeric
+  expect_that( is.numeric(temp_F), equals(TRUE) );
+  
+  # Test that the result is the correct value
+  expect_that( temp_F, equals(50) );
+})
+
+
+# The two tests can be run on the command line as below
+# test_dir(".")
