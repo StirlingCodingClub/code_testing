8b1f150c78ac10391766fb2ec3aac5f284ea5af6
Brad Duthie <brad.duthie@gmail.com>
2023-03-19 19:06:29 +0000

    Update notes

--- test-temp_conversion.R
+++ test-temp_conversion.R
@@ -27,6 +27,7 @@ test_that("Celsius to Fahrenheit", {
 # This test will fail
 test_that(desc = "Fahrenheit to Celsius wrong", code = {
     temp_F <- F_to_C(50);
+    
     expect_that( object = temp_F, condition = equals(2) );
 })
 
